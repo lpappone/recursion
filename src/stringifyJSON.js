116270fbeb2c622941bfa759112a3dbf5c7bbe8b
Lauren <lpappone@gmail.com>
2014-06-17 22:50:56 -0700

    working on basic nested arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,61 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var finalresult = [];
+  var placeholder
+
+  function checkType(x) {
+    if(typeof x == "number" || typeof x == "boolean" || x == null) {
+      // finalresult.push(String(x));
+      // return finalresult.join();
+      placeholder = String(x);
+      return placeholder;
+    }
+    if(typeof x == "string") {
+      // finalresult.push(x);
+      // return finalresult.join();
+      placeholder = ['"' + x + '"'].join();
+      return placeholder
+    }
+    if(typeof x == "object") {
+      return runThroughObject(x);
+    }
+  };
+  
+  function runThroughObject(obj) {
+    var keys = [];
+    var values = [];
+    var results = [];
+    if(Array.isArray(obj) == false) {
+      for(var k in obj) {
+        keys.push(String(k));
+      }
+      for(i = 0; i < keys.length; i++) {
+        var thing = checkType((obj[keys[i]]));
+        values.push(String(thing));
+      }
+      for(i = 0; i <keys.length; i++) {
+        results.push(keys[i]);
+        results.push(": ");
+        results.push(values[i]);
+      }
+      finalresult.push("{" + results.join("") + "}");
+    } else {
+        for (i = 0; i < obj.length; i++) {
+          if (Array.isArray(obj[i]) == false) {
+            finalresult.push("[");
+            //if(i > 0) {results.push(",");}
+            var thing = checkType(obj[i]);
+            results.push(thing + ",");
+            if(i >= obj.length - 1) {results.push("]")}
+            finalresult.push(results.join(""));
+          } else { 
+            runThroughObject(obj[i]);
+          }
+          
+        }
+    }
+    return finalresult.join("");
+  };
+  return checkType(obj);
 };
