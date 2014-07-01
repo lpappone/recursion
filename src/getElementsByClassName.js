f09893a739d93f829f599d08f85ec48d035cfa3f
lpappone <lpappone@gmail.com>
2014-07-01 15:07:17 -0700

    fixed commented out section

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,37 +5,37 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // var results = [];
-  // var walkDOM = function(node) {
-  // 	if (node.classList !== undefined && node.classList.contains(className)) {
-  // 		results.push(node);
-  // 	}
-  // 	if (node.hasChildNodes) {
-  // 		var children = node.childNodes;
-  // 		for (var i = 0; i < children.length; i++) {
-  // 			walkDOM(children[i]);
-  // 		}
-  // 	}
-  // }
-  // walkDOM(document.body);
-  // return results;
-
   var results = [];
-  var getKids = function(node) {
-    if (node.classList !== undefined && node.classList.contains(className)) {
-      results.push(node);
-    }
-    if (node.hasChildNodes) {
-      var children = node.childNodes;
-      for (var i = 0; i < children.length; i ++) {
-        if (children[i].classList !== undefined && children[i].classList.contains(className)) {
-          results.push(children[i]);
-        }
-      for (var i = 0; i < children.length; i ++) {
-        getKids(children[i]);
-      }
-      }
-    }
-  };
-  return getKids(document.body);
+  var walkDOM = function(node) {
+  	if (node.classList !== undefined && node.classList.contains(className)) {
+  		results.push(node);
+  	}
+  	if (node.hasChildNodes) {
+  		var children = node.childNodes;
+  		for (var i = 0; i < children.length; i++) {
+  			walkDOM(children[i]);
+  		}
+  	}
+  }
+  walkDOM(document.body);
+  return results;
+
+  // var results = [];
+  // var getKids = function(node) {
+  //   if (node.classList !== undefined && node.classList.contains(className)) {
+  //     results.push(node);
+  //   }
+  //   if (node.hasChildNodes) {
+  //     var children = node.childNodes;
+  //     for (var i = 0; i < children.length; i ++) {
+  //       if (children[i].classList !== undefined && children[i].classList.contains(className)) {
+  //         results.push(children[i]);
+  //       }
+  //     for (var i = 0; i < children.length; i ++) {
+  //       getKids(children[i]);
+  //     }
+  //     }
+  //   }
+  // };
+  // return getKids(document.body);
 };
